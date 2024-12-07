5164baa08580606772ef99a4429dd273c486f17e
Victor Lin <13424970+victorlin@users.noreply.github.com>
2024-12-07 12:45:07 -0800

    simplify app and TSConfig
    Move everything to App.ts and remove unnecessary class.

--- App.ts
+++ App.ts
@@ -1,4 +1,31 @@
-import { app, BrowserWindow } from 'electron';
-import Main from './Main';
+import {app, BrowserWindow, shell} from 'electron'
 
-Main.main(app, BrowserWindow);
+function createWindow(url: string) {
+    console.log(`Opening URL in a new window: ${url}`)
+    
+    const window = new BrowserWindow()
+    window.maximize()
+    window.loadURL(url)
+
+    window.webContents.setWindowOpenHandler(({url}) => {
+        if (url.includes('google.com') && !url.startsWith('https://www.google.com/url')) {
+            createWindow(url)
+            return { action: 'deny' }
+        } else {
+            console.log(`Opening URL in default web browser: ${url}`)
+            shell.openExternal(url)
+                .then(() => {})
+            return { action: 'deny' }
+        }
+    })
+
+    return window
+}
+
+app.on('window-all-closed', () => {
+    app.quit()
+})
+
+app.whenReady().then(() => {
+    createWindow('https://mail.google.com')
+})
