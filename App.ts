bbf36a623c74e00aaeca1e5d437771b54c65c6f4
Victor Lin <13424970+victorlin@users.noreply.github.com>
2025-01-09 16:29:18 -0800

    open new window for URL input

--- App.ts
+++ App.ts
@@ -1,4 +1,4 @@
-import {app, BrowserWindow, shell} from 'electron'
+import {app, BrowserWindow, globalShortcut, shell} from 'electron'
 
 function createWindow(url: string) {
     console.log(`Opening URL in a new window: ${url}`)
@@ -26,6 +26,32 @@ app.on('window-all-closed', () => {
     app.quit()
 })
 
+function openNewUrlWindow() {
+    console.log(`Opening an empty window.`)
+    const window = new BrowserWindow()
+    window.maximize()
+    window.loadURL(`data:text/html, <body>
+        <input type="text" id="input" style="width: 100%; height: 100%; box-sizing: border-box; padding: 10px; font-size: 16px;" autofocus/>
+        <script>
+          const input = document.getElementById('input');
+          input.addEventListener('keyup', (event) => {
+            if (event.key === 'Enter') {
+              const url = input.value.trim();
+              if (url) {
+                window.location.href = url;
+              }
+            }
+          });
+        </script>
+    </body>`)
+
+    window.once('ready-to-show', () => {
+        window.show()
+        window.webContents.focus()
+    })
+}
+
 app.whenReady().then(() => {
     createWindow('https://mail.google.com')
+    globalShortcut.register('Command+N', openNewUrlWindow)
 })
